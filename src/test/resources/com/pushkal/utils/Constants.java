7e3f14bb510708849a9c8ccd660cd42f9c2b29c9
Pushkal_Singh <Pushkal_Singh@EPINPUNW000B.princeton.epam.com>
2020-02-26 15:55:52 +0530

    added constants.java
    the file will consist the parameters used in framework

---
+++ src/test/resources/com/pushkal/utils/Constants.java
@@ -0,0 +1,6 @@
+
+public class Constants {
+	public static final String baseURI = "https://gorest.co.in";
+	public static final String basePath = "/public-api";
+	public static final String authToken = "VyjTtuDokqeKk9uwdV1Hwoee9LJzj18cwOTf";
+}

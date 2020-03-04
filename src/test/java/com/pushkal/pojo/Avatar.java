43746ce5290b122437a2366329230d01f99c58de
Pushkal_Singh <Pushkal_Singh@EPINPUNW000B.princeton.epam.com>
2020-03-04 15:43:44 +0530

    POJOs for JSONs added to the framework

---
+++ src/test/java/com/pushkal/pojo/Avatar.java
@@ -0,0 +1,19 @@
+package com.pushkal.pojo;
+
+public class Avatar {
+
+	private String href;
+
+	public Avatar(String href) {
+		this.href = href;
+	}
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+}
---
+++ src/test/java/com/pushkal/pojo/Self.java
@@ -0,0 +1,21 @@
+package com.pushkal.pojo;
+
+public class Self {
+
+	private String href;
+
+	public Self(String href) {
+		this.href = href;
+	}
+
+	public String getHref() {
+		return href;
+	}
+
+	public void setHref(String href) {
+		this.href = href;
+	}
+
+	
+
+}
